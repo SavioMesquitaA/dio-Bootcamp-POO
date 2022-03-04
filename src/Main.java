e8b7314dbd1b7b469e04407c72b0ddcd15786565
SavioMesquitaA <of.saviomesquita1406@gmail.com>
2022-03-04 09:26:28 -0300

    new commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso de Java");
+        curso1.setCargaHoraria(5);
+
+        System.out.println(curso1);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("Mentodria de Java");
+        mentoria.setCargaHoraria(LocalDate.now());
+
+        System.out.println(mentoria);
 
     }
 }
