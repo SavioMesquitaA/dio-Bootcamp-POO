f6d99eb7c238cce5db3892a281b21dc6c7b9e443
SavioMesquitaA <of.saviomesquita1406@gmail.com>
2022-03-04 11:07:10 -0300

    "Final" commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -12,14 +10,36 @@ public class Main {
         curso1.setDescricao("Curso de Java");
         curso1.setCargaHoraria(5);
 
-        System.out.println(curso1);
-
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Java");
         mentoria.setDescricao("Mentodria de Java");
         mentoria.setCargaHoraria(LocalDate.now());
 
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("GFT Start #4");
+        bootcamp.setDescricao("Desrição do Bootcamp Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev desenvolvedor1, desenvolvedor2;
+
+        desenvolvedor1 = new Dev();
+        desenvolvedor1.setNome("Savio");
+        desenvolvedor1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de "+desenvolvedor1.getNome()
+                +" "+desenvolvedor1.getConteudosInscritos());
+        desenvolvedor1.Progredir();
+        desenvolvedor1.Progredir();
+        System.out.println(" - ");
+        System.out.println("Conteudos inscritos "+desenvolvedor1.getConteudosInscritos());
+        System.out.println("Conteudos concluidos "+desenvolvedor1.getConteudosComcluidos());
+        System.out.println("XP: "+desenvolvedor1.calcularTotalXP());
+
+
+        desenvolvedor2 = new Dev();
+        desenvolvedor2.setNome("Katia");
+        desenvolvedor2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos "+desenvolvedor2.getConteudosInscritos());
 
     }
 }
